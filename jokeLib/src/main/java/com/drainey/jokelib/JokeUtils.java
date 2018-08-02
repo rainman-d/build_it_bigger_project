d1be9b0e72b910eca776bc703f249511d21a3b23
David Rainey <david.rainey83@gmail.com>
2018-08-01 20:11:53 -0500

    Step 1 finished

---
+++ jokeLib/src/main/java/com/drainey/jokelib/JokeUtils.java
@@ -0,0 +1,7 @@
+package com.drainey.jokelib;
+
+public class JokeUtils {
+    public static String getJoke(){
+        return "How do you kill a vegetarian vampire?\nWith a steak to the heart!";
+    }
+}
